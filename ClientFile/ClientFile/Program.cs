072f5deff0b87c2732a02a6199756c5f3ac83768
Садыкова Аида Хаирбекова <@corp.itstep>
2018-06-05 11:26:20 +0600

    file move

---
+++ ClientFile/ClientFile/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientFile
+{
+    class Program
+    {
+        private static int defaultPort = 3535;
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), defaultPort);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+
+                Console.WriteLine("Client запущен.. ");
+                Console.WriteLine("отправка сообщения");
+                socket.Connect(endPoint);
+                Console.WriteLine("отправте путь файла :");
+                string text = @"d:\1.txt";
+                //string.text = Console.ReadLine();
+                socket.Send(Encoding.Default.GetBytes(text));
+                Console.WriteLine("Отправлен");
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                socket.Close();
+            }
+            Console.ReadLine();
+
+        }
+       
+    }
+}
---
+++ ServerFile/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerFile
+{
+    class Program
+    {
+        private static int defaultPort = 3535;
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), defaultPort);
+            try
+            {
+                server.Start(); string newPath = @"d:\121\2.txt";
+
+                while (true)
+                {
+                    Console.WriteLine("Сервер запущен..");
+         
+                    TcpClient client = server.AcceptTcpClient();
+                    File.Move(GetClientData(client).Result, newPath);
+                    Console.WriteLine("Файл перемещен и переименован");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+
+      
+        public static Task<string> GetClientData(TcpClient client)
+        {
+            int bytes;
+                using (var networkStream = client.GetStream())
+                {
+                  byte[] data = new byte[512];
+                    do
+                    {
+                        bytes = networkStream.Read(data, 0, data.Length);
+                        string path = Encoding.Default.GetString(data, 0, bytes);
+                        return Task.Run(
+                            () =>
+                            {
+                            return path;
+                            });
+                }
+                    while (networkStream.DataAvailable);
+                }
+            
+           
+        }
+
+
+    }
+
+}
